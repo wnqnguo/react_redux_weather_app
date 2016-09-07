d0ad197b107aabe6f0dee01f745a30f659bbd53d
Wenqian Guo <wnqnguo@gmail.com>
2016-09-07 16:19:24 -0700

    redux promise and request url

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,9 @@
 const API_KEY ='e81dac8fc944ad351d9927c3ce4ae313'
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export function fetchWeather(city){
+	const url=`${ROOT_URL}&q={city},us`
+	return{
+		type: FETCH_WEATHER
+	}
+}
