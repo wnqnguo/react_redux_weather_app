6d4aac8ed5acffa627cbe61714b09484bfdfe0d4
Wenqian Guo <wnqnguo@gmail.com>
2016-09-07 16:50:52 -0700

    add weather to reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,6 +5,7 @@ const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KE
 export function fetchWeather(city){
 	const url=`${ROOT_URL}&q={city},us`;
 	const request = axios.get(url);
+	console.log(' Request:', request);
 	return{
 		type: FETCH_WEATHER,
 		payload: request
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,12 +1,13 @@
 import React,{Component} from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreator } from 'redux';
+import { bindActionCreators } from 'redux';
 import { fetchWeather } from '../actions/index';
  class SearchBar extends Component{
 	constructor(props){
 		super(props);
 		this.state ={term: ''};
 		this.onInputChange = this.onInputChange.bind(this);
+		this.onFormSubmit = this.onFormSubmit.bind(this);
 	}
 	onInputChange(event){
 		console.log(event.target.value);
